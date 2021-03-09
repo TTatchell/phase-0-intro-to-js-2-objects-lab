c2357bc1252a0c2d3be115f3ee9eaa17d811ab1f
TTatchell <tim.tatchell24@gmail.com>
2021-03-09 18:15:28 +1100

    upload

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Write your solution in this file!
+const employee = {
+    name: "Tim",
+    streetAddress: "McDougall Avenue",
+}
+
+function updateEmployeeWithKeyAndValue(emp, key, value) {
+    const newObj = {...emp};
+    newObj[key] = value;
+    return newObj
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(emp, key, value) {
+    emp[key] = value;
+    return emp
+}
+
+function deleteFromEmployeeByKey(emp, key) {
+    const newEmp = {...emp};
+    delete newEmp[key];
+    return newEmp
+
+}
+
+function destructivelyDeleteFromEmployeeByKey(emp, key) {
+    delete emp[key];
+    return emp
+
+}
